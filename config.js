342df9e119fb42a148f78c62b1085d9067d0346e
amine daudo <adaddasmaravilhas@gmail.com>
2026-09-17 10:24:21 +0200

    Load persistent invites and dice rule UI

--- config.js
+++ config.js
@@ -23,5 +23,6 @@ window.JL_CONFIG = Object.freeze({
     add('./ludo-public-challenges-v3.js?v=1');
     add('./ludo-room-flow-v2.js?v=2');
     add('./ludo-authoritative-sync.js?v=1');
+    add('./ludo-rules-options-v2.js?v=1');
   }
 })();
